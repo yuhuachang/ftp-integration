4d195d623930d3f740220b6cf145e9f2c091f6a2
Yu-Hua Chang <18146296+yuhuachang@users.noreply.github.com>
2018-09-26 00:16:37 +0800

    finish inbound flow tests

--- src/test/java/com/example/ftpintegration/InboundProcessingFlowTest.java
+++ src/test/java/com/example/ftpintegration/InboundProcessingFlowTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
-import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.junit.Before;
 import org.junit.Test;
@@ -355,7 +353,7 @@ public class InboundProcessingFlowTest {
         // get file content
         verify(op, times(1)).retrieveFile("/input/mockFile");
 
-        // trigger success result
+        // trigger error result
         verify(synchronizer, times(1)).onFtpError(any(), any());
 
         verifyNoMoreInteractions(op);
@@ -364,7 +362,7 @@ public class InboundProcessingFlowTest {
     }
 
     /**
-     * Test single file read error.
+     * Test single file processing error.
      * 
      * @throws Exception
      */
@@ -410,11 +408,496 @@ public class InboundProcessingFlowTest {
         // get file content
         verify(op, times(1)).retrieveFile("/input/mockFile");
 
-        // trigger success result
+        // process file
+        verify(processor, times(1)).processFile(content);
+
+        // trigger error result
         verify(synchronizer, times(1)).onFileError(eq("/input/mockFile"), eq(content), any(), any());
 
         verifyNoMoreInteractions(op);
         verifyNoMoreInteractions(processor);
         verifyNoMoreInteractions(synchronizer);
     }
+
+    /**
+     * Test single file upload error.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void uploadError() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file = mock(FTPFile.class);
+        when(file.getName()).thenReturn("mockFile");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file });
+        when(file.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content = "test".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile"))).thenReturn(content);
+
+        // upload error
+        doThrow(IOException.class).when(op).storeFile("/archive/mockFile", content);
+
+        // run and has error. will not throw exception out because it will continue
+        // processing next file.
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(1);
+
+        // get file info
+        verify(file, times(1)).isDirectory();
+        verify(file, times(1)).getName();
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile");
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile");
+
+        // process file
+        verify(processor, times(1)).processFile(content);
+
+        // upload file
+        verify(op, times(1)).storeFile("/archive/mockFile", content);
+
+        // trigger error result
+        verify(synchronizer, times(1)).onFtpError(any(), any());
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+    
+    /**
+     * Test single file delete error.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void deleteError() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file = mock(FTPFile.class);
+        when(file.getName()).thenReturn("mockFile");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file });
+        when(file.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content = "test".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile"))).thenReturn(content);
+
+        // upload error
+        doThrow(IOException.class).when(op).deleteFile("/input/mockFile");
+
+        // run and has error. will not throw exception out because it will continue
+        // processing next file.
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(1);
+
+        // get file info
+        verify(file, times(1)).isDirectory();
+        verify(file, times(1)).getName();
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile");
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile");
+
+        // process file
+        verify(processor, times(1)).processFile(content);
+
+        // upload file
+        verify(op, times(1)).storeFile("/archive/mockFile", content);
+
+        // delete file
+        verify(op, times(1)).deleteFile("/input/mockFile");
+
+        // trigger error result
+        verify(synchronizer, times(1)).onFtpError(any(), any());
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
+    /**
+     * Two files correct processing flow.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void processTwoFiles() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file1 = mock(FTPFile.class);
+        when(file1.getName()).thenReturn("mockFile1");
+        FTPFile file2 = mock(FTPFile.class);
+        when(file2.getName()).thenReturn("mockFile2");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file1, file2 });
+        when(file1.isDirectory()).thenReturn(false);
+        when(file2.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content1 = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] content2 = "1234".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile1"))).thenReturn(content1);
+        when(op.retrieveFile(eq("/input/mockFile2"))).thenReturn(content2);
+
+        // run
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(2);
+
+        // get file info
+        verify(file1, times(1)).isDirectory();
+        verify(file2, times(1)).isDirectory();
+        verify(file1, times(1)).getName();
+        verify(file2, times(1)).getName();
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile1");
+        verify(op, times(1)).retrieveFile("/input/mockFile2");
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile1");
+        verify(synchronizer, times(1)).onStart("/input/mockFile2");
+        verify(processor, times(1)).processFile(content1);
+        verify(processor, times(1)).processFile(content2);
+
+        // store file to archive folder
+        verify(op, times(1)).storeFile("/archive/mockFile1", content1);
+        verify(op, times(1)).storeFile("/archive/mockFile2", content2);
+
+        // delete input file
+        verify(op, times(1)).deleteFile("/input/mockFile1");
+        verify(op, times(1)).deleteFile("/input/mockFile2");
+
+        // trigger success result
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile1");
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile2");
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
+    /**
+     * Two files, the first one failed, the second one success.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void processTwoFilesOneErrorOnRead() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file1 = mock(FTPFile.class);
+        when(file1.getName()).thenReturn("mockFile1");
+        FTPFile file2 = mock(FTPFile.class);
+        when(file2.getName()).thenReturn("mockFile2");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file1, file2 });
+        when(file1.isDirectory()).thenReturn(false);
+        when(file2.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content2 = "test".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        doThrow(IOException.class).when(op).retrieveFile(eq("/input/mockFile1"));
+        when(op.retrieveFile(eq("/input/mockFile2"))).thenReturn(content2);
+
+        // run
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(2);
+
+        // get file info
+        verify(file1, times(1)).isDirectory();
+        verify(file2, times(1)).isDirectory();
+        verify(file1, times(1)).getName();
+        verify(file2, times(1)).getName();
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile1");
+        verify(op, times(1)).retrieveFile("/input/mockFile2");
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile1");
+        verify(synchronizer, times(1)).onStart("/input/mockFile2");
+        verify(processor, times(1)).processFile(content2);
+
+        // store file to archive folder
+        verify(op, times(1)).storeFile("/archive/mockFile2", content2);
+
+        // delete input file
+        verify(op, times(1)).deleteFile("/input/mockFile2");
+
+        // trigger success result
+        verify(synchronizer, times(1)).onFtpError(any(), any());
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile2");
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
+    /**
+     * Two files, the first one failed, the second one success.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void processTwoFilesOneErrorOnProcess() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file1 = mock(FTPFile.class);
+        when(file1.getName()).thenReturn("mockFile1");
+        FTPFile file2 = mock(FTPFile.class);
+        when(file2.getName()).thenReturn("mockFile2");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file1, file2 });
+        when(file1.isDirectory()).thenReturn(false);
+        when(file2.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content1 = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] content2 = "1234".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile1"))).thenReturn(content1);
+        when(op.retrieveFile(eq("/input/mockFile2"))).thenReturn(content2);
+
+        doThrow(IOException.class).when(processor).processFile(content1);
+        
+        // run
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(2);
+
+        // get file info
+        verify(file1, times(1)).isDirectory();
+        verify(file2, times(1)).isDirectory();
+        verify(file1, times(1)).getName();
+        verify(file2, times(1)).getName();
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile1");
+        verify(op, times(1)).retrieveFile("/input/mockFile2");
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile1");
+        verify(synchronizer, times(1)).onStart("/input/mockFile2");
+        verify(processor, times(1)).processFile(content1);
+        verify(processor, times(1)).processFile(content2);
+
+        // store file to archive folder
+        verify(op, times(1)).storeFile("/archive/mockFile2", content2);
+
+        // delete input file
+        verify(op, times(1)).deleteFile("/input/mockFile2");
+
+        // trigger success result
+        verify(synchronizer, times(1)).onFileError(eq("/input/mockFile1"), eq(content1), any(), any());
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile2");
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
+    /**
+     * Two files, the first one failed, the second one success.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void processTwoFilesOneErrorOnUpload() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file1 = mock(FTPFile.class);
+        when(file1.getName()).thenReturn("mockFile1");
+        FTPFile file2 = mock(FTPFile.class);
+        when(file2.getName()).thenReturn("mockFile2");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file1, file2 });
+        when(file1.isDirectory()).thenReturn(false);
+        when(file2.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content1 = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] content2 = "1234".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile1"))).thenReturn(content1);
+        when(op.retrieveFile(eq("/input/mockFile2"))).thenReturn(content2);
+
+        // die on upload
+        doThrow(IOException.class).when(op).storeFile("/archive/mockFile1", content1);
+
+        // run
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(2);
+
+        // get file info
+        verify(file1, times(1)).isDirectory();
+        verify(file2, times(1)).isDirectory();
+        verify(file1, times(1)).getName();
+        verify(file2, times(1)).getName();
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile1");
+        verify(op, times(1)).retrieveFile("/input/mockFile2");
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile1");
+        verify(synchronizer, times(1)).onStart("/input/mockFile2");
+        verify(processor, times(1)).processFile(content1);
+        verify(processor, times(1)).processFile(content2);
+
+        // store file to archive folder
+        verify(op, times(1)).storeFile("/archive/mockFile1", content1);
+        verify(op, times(1)).storeFile("/archive/mockFile2", content2);
+
+        // delete input file
+        verify(op, times(1)).deleteFile("/input/mockFile2");
+
+        // trigger success result
+        verify(synchronizer, times(1)).onFtpError(any(), any());
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile2");
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
+    /**
+     * Two files correct processing flow.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void processTwoFilesOneErrorOnDelete() throws Exception {
+
+        // a real flow object
+        InboundProcessingFlow flow = new InboundProcessingFlow("/input", "/archive", processor);
+        flow.setFtpFlowSynchronizer(synchronizer);
+
+        // the mocked file to use
+        FTPFile file1 = mock(FTPFile.class);
+        when(file1.getName()).thenReturn("mockFile1");
+        FTPFile file2 = mock(FTPFile.class);
+        when(file2.getName()).thenReturn("mockFile2");
+
+        // list files and return one file.
+        when(op.listFiles(anyString())).thenReturn(new FTPFile[] { file1, file2 });
+        when(file1.isDirectory()).thenReturn(false);
+        when(file2.isDirectory()).thenReturn(false);
+
+        // test file content
+        byte[] content1 = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] content2 = "1234".getBytes(StandardCharsets.UTF_8);
+
+        // retrieve file content success
+        when(op.retrieveFile(eq("/input/mockFile1"))).thenReturn(content1);
+        when(op.retrieveFile(eq("/input/mockFile2"))).thenReturn(content2);
+
+        // die on delete
+        doThrow(IOException.class).when(op).deleteFile("/input/mockFile1");
+
+        // run
+        flow.execute(op);
+
+        // list input folder
+        verify(op, times(1)).listFiles("/input");
+        verify(synchronizer, times(1)).onListing(2);
+
+        // get file info
+        verify(file1, times(1)).isDirectory();
+        verify(file2, times(1)).isDirectory();
+        verify(file1, times(1)).getName();
+        verify(file2, times(1)).getName();
+
+        // get file content
+        verify(op, times(1)).retrieveFile("/input/mockFile1");
+        verify(op, times(1)).retrieveFile("/input/mockFile2");
+
+        // start processing...
+        verify(synchronizer, times(1)).onStart("/input/mockFile1");
+        verify(synchronizer, times(1)).onStart("/input/mockFile2");
+        verify(processor, times(1)).processFile(content1);
+        verify(processor, times(1)).processFile(content2);
+
+        // store file to archive folder
+        verify(op, times(1)).storeFile("/archive/mockFile1", content1);
+        verify(op, times(1)).storeFile("/archive/mockFile2", content2);
+
+        // delete input file
+        verify(op, times(1)).deleteFile("/input/mockFile1");
+        verify(op, times(1)).deleteFile("/input/mockFile2");
+
+        // trigger success result
+        verify(synchronizer, times(1)).onFtpError(any(), any());
+        verify(synchronizer, times(1)).onSuccess("/input/mockFile2");
+
+        verifyNoMoreInteractions(op);
+        verifyNoMoreInteractions(processor);
+        verifyNoMoreInteractions(synchronizer);
+    }
+
 }
